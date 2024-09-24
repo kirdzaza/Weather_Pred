218b6ae954197c7ed40ba91e2b4957767f183536
kirdzaza <148518495+kirdzaza@users.noreply.github.com>
2024-09-24 18:53:35 +0700

    Update airQuality.js

--- app/api/airQuality.js
+++ app/api/airQuality.js
@@ -4,7 +4,7 @@ export default async function getWeather(lat, lon) {
   const apiKey = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`
+      `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`
     );
 
     if (!response.ok) {
